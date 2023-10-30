8bd4c64e8dc610ef186663803cb980172d052d53
NAMDP-224012\ealflm <ealflm@gmail.com>
2023-10-30 12:10:31 +0700

    commit

--- lua/telescope/_extensions/nvwork.lua
+++ lua/telescope/_extensions/nvwork.lua
@@ -7,18 +7,38 @@ local sorters = require("telescope.sorters")
 local make_entry = require("telescope.make_entry")
 local api = vim.api
 
+local cwd = vim.fn.expand("~/.nvwork/")
+
 return require("telescope").register_extension {
   exports = {
     select = function()
       local opts = {
-        cwd = vim.fn.expand("~/.nvwork/"),
+        cwd = cwd,
         attach_mappings = function(prompt_bufnr, map)
+          -- enter action
           actions.select_default:replace(function()
             actions.close(prompt_bufnr)
             local selection = action_state.get_selected_entry()
             local file_path = getmetatable(selection).cwd .. selection[1]
             require("nvwork").set_nvwork_selected_file(file_path)
           end)
+
+          -- ctrl + enter action
+          local create_new = function()
+            local search_text = action_state.get_current_line()
+            actions.close(prompt_bufnr)
+
+            local file_path = cwd .. search_text
+
+            local create_cmd = "silent !New-Item -ItemType File -Force -Path " .. file_path
+            vim.cmd(create_cmd)
+
+            require("nvwork").set_nvwork_selected_file(file_path)
+          end
+
+          map('i', '<A-CR>', create_new)
+          map('n', '<A-CR>', create_new)
+
           return true
         end,
       }
